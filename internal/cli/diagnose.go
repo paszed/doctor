d54ddb4143d9522ced1c554e59ce13c00c532967
paszed <edvard.pasz@icloud.com>
2026-03-17 16:05:58 +0100

    feat: add exit codes to diagnose command

--- internal/cli/diagnose.go
+++ internal/cli/diagnose.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/paszed/doctor/internal/checks"
+	"github.com/paszed/doctor/internal/model"
 	"github.com/paszed/doctor/internal/ui"
 )
 
@@ -13,12 +14,50 @@ func RunDiagnose() {
 
 	results := checks.Diagnose()
 
+	// JSON mode
 	if len(os.Args) > 2 && os.Args[2] == "--json" {
+		data, err := json.MarshalIndent(results, "", "  ")
+		if err != nil {
+			fmt.Println("failed to encode json:", err)
+			os.Exit(1)
+		}
 
-		data, _ := json.MarshalIndent(results, "", "  ")
 		fmt.Println(string(data))
 		return
 	}
 
+	// Human readable output
 	ui.PrintResults(results)
+
+	// Exit code calculation
+	ok := 0
+	warn := 0
+	missing := 0
+
+	for _, r := range results {
+
+		switch r.Status {
+
+		case model.OK:
+			ok++
+
+		case model.Warning:
+			warn++
+
+		case model.Missing:
+			missing++
+
+		}
+	}
+
+	// Exit codes
+	if missing > 0 {
+		os.Exit(2)
+	}
+
+	if warn > 0 {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
